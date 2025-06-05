17e1f0fc460d14fa8e55803e400a0fa6ed0dc206
barani <paranidharan92@gmail.com>
2025-06-05 22:55:31 +0530

    d

--- flask/app.py
+++ flask/app.py
@@ -1,49 +1,103 @@
-from flask import Flask, jsonify, request, render_template_string
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3, os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/images'
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # Max 2MB
 
-# A simple in-memory structure to store tasks
-tasks = []
+def init_db():
+    conn = sqlite3.connect('database1.db')
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS stock (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            purchase_price REAL,
+            selling_price REAL,
+            quantity INTEGER,
+            image TEXT
+        )
+    ''')
+    conn.close()
 
-@app.route('/', methods=['GET'])
-def home():
-    # Display existing tasks and a form to add a new task
-    html = '''
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Todo List</title>
-</head>
-<body>
-    <h1>Todo List</h1>
-    <form action="/add" method="POST">
-        <input type="text" name="task" placeholder="Enter a new task">
-        <input type="submit" value="Add Task">
-    </form>
-    <ul>
-        {% for task in tasks %}
-        <li>{{ task }} <a href="/delete/{{ loop.index0 }}">x</a></li>
-        {% endfor %}
-    </ul>
-</body>
-</html>
-'''
-    return render_template_string(html, tasks=tasks)
+@app.route('/')
+def index():
+    conn = sqlite3.connect('database1.db')
+    items = conn.execute("SELECT * FROM stock").fetchall()
+    conn.close()
+    return render_template('index.html', items=items)
 
 @app.route('/add', methods=['POST'])
-def add_task():
-    # Add a new task from the form data
-    task = request.form.get('task')
-    if task:
-        tasks.append(task)
-    return home()
+def add():
+    name = request.form['name']
+    purchase_price = float(request.form['purchase_price'])
+    selling_price = float(request.form['selling_price'])
+    quantity = int(request.form['quantity'])
 
-@app.route('/delete/<int:index>', methods=['GET'])
-def delete_task(index):
-    # Delete a task based on its index
-    if index < len(tasks):
-        tasks.pop(index)
-    return home()
+    image_file = request.files['image']
+    image_name = ''
+    if image_file and image_file.filename != '':
+        image_name = secure_filename(image_file.filename)
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_name)
+        image_file.save(image_path)
+
+    conn = sqlite3.connect('database1.db')
+    conn.execute("INSERT INTO stock (name, purchase_price, selling_price, quantity, image) VALUES (?, ?, ?, ?, ?)",
+                 (name, purchase_price, selling_price, quantity, image_name))
+    conn.commit()
+    conn.close()
+    return redirect('/')
+
+@app.route('/edit/<int:item_id>', methods=['GET', 'POST'])
+def edit(item_id):
+    conn = sqlite3.connect('database1.db')
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+        name = request.form['name']
+        purchase_price = float(request.form['purchase_price'])
+        selling_price = float(request.form['selling_price'])
+        quantity = int(request.form['quantity'])
+
+        image_file = request.files['image']
+        if image_file and image_file.filename != '':
+            image_name = secure_filename(image_file.filename)
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_name)
+            image_file.save(image_path)
+        else:
+            image_name = request.form['current_image']
+
+        cursor.execute('''
+            UPDATE stock SET name=?, purchase_price=?, selling_price=?, quantity=?, image=? WHERE id=?
+        ''', (name, purchase_price, selling_price, quantity, image_name, item_id))
+
+        conn.commit()
+        conn.close()
+        return redirect('/')
+
+    cursor.execute("SELECT * FROM stock WHERE id=?", (item_id,))
+    item = cursor.fetchone()
+    conn.close()
+    return render_template('edit.html', item=item)
+
+@app.route('/delete/<int:item_id>')
+def delete(item_id):
+    conn = sqlite3.connect('database1.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT image FROM stock WHERE id=?", (item_id,))
+    image = cursor.fetchone()
+    if image and image[0]:
+        try:
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], image[0]))
+        except:
+            pass
+    cursor.execute("DELETE FROM stock WHERE id=?", (item_id,))
+    conn.commit()
+    conn.close()
+    return redirect('/')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    init_db()
+    if not os.path.exists('static/images'):
+        os.makedirs('static/images')
+    app.run(debug=True,host="0.0.0.0")
